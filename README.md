2adbf14994e61c65e3837df40bef3a5639320bb5
Andreas Sekulski <andreas.sekulski@gmail.com>
2021-07-04 12:49:28 +0200

    Gradle project configured and eureka server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SpringCloudMicroservices
 Example project to create multiple microservices by spring-cloud and eureka
+
+Goal from this project is to migrate the V18N example as microservice example -> https://github.com/Nepitwin/V18N
---
+++ eureka/src/main/java/com/asekulsk/eureka/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.asekulsk.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka discovery service for microservice modules to attach.
+ *
+ * @author Andreas Sekulski
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
